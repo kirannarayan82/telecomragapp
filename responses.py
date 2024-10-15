6181966f57258d76d781ad5924e5595e92865393
ksathyanarayana <genuser0482@outlook.com>
2024-10-15 09:22:18 +0530

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,5 @@
+To reset your password, go to the login page and click on "Forgot Password". Follow the instructions to reset your password.
+You can track your order by logging into your account and going to the "Orders" section. Enter your order ID to get the latest status.
+To return a product, visit our returns page and fill out the return form. Follow the instructions to send the product back.
+You can contact our customer support by calling 123-456-7890 or emailing support@example.com.
+To update your profile information, log in to your account and go to the "Profile" section. Edit the information and save the changes.
